58ba284d4cfe084fc1c4d1078ad403d5a1034bdb
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2023-10-23 23:46:49 +0200

    main commit all is done

--- ios/OneSignalNotificationServiceExtension/NotificationService.h
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -1,3 +1,10 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Ivan Labasok on 23/10/2023.
+//
+
 #import <UserNotifications/UserNotifications.h>
 
 @interface NotificationService : UNNotificationServiceExtension
